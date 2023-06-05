9cfd6c55345817b5b0a5e2ea5b2708fd348740a9
unknown <pontoni.emanuel@gmail.com>
2023-06-04 22:22:27 -0300

    test 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function share(obj) {
+  if(navigator.share) {
+    navigator
+      .share(obj)
+      .then(() => console.log('Ok'))
+      .catch(error => console.log('Error', error));
+  } else {
+    console.log('No hay soporte para compartir.')
+  }
+}
 
 function App() {
+
+  // Create a random image and then convert it to Blob finally to a file
+  const [file, setFile] = useState(null);
+  useEffect(() => {
+    async function getImages() {
+      const img = await fetch('https://picsum.photos/200/300');
+      const blob = await img.blob();
+      const file = new File([blob], 'story.png', { type: 'image/png'});
+      setFile(file)
+    }
+    getImages();
+  }, []);
+
+  // objects to share info
+
+  const story = {
+    title: 'Titulo',
+    text: 'descripcion',
+    url: 'https://epontoni.github.io',
+    files: [file],
+  };
+
+  // Share API
+  https://developer.mozilla.org/en-US/docs/Web/API/Navigator/share
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={ () => share(story) }>Share</button>
     </div>
   );
 }
